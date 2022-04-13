07ef3299537999e981f5ea94ce3496398211349c
kelly <kelly920508@gmail.com>
2022-04-13 13:13:59 +0800

    lastt

--- portfolio-1/portfolio-1/excel/main.js
+++ portfolio-1/portfolio-1/excel/main.js
@@ -5,11 +5,10 @@ $(function(){
     //每分鐘60秒、每小時60分鐘、每天24小時
     let millisecsPerDay = 24*60*60*1000;
     for(var x=0;x<topicCount;x++){
-    $("#courseTable").append("<tr>"
+    $("#courseTable").append("<tr>"+
     `<td>${x+1}</td>`+
     `<td>${(new Date(startDate.getTime()+7*x*millisecsPerDay)).toLocaleDateString().slice(5)}</td>`+
     `<td>${topic[x]}</td>`+
     "</tr>");
     }
-
     });
--- portfolio-1/portfolio-1/excel/topic.js
+++ portfolio-1/portfolio-1/excel/topic.js
@@ -5,13 +5,13 @@ var topic = [
     "隨機性",
     "重複性",
     "條件判斷"
-    ];
-    var startDate = new Date();
-function setMonthAndDay(startMonth, startDay){
-//一次設定好月份與日期
-startDate.setMonth(startMonth-1,startDay);
-startDate.setHours(0);
-startDate.setMinutes(0);
-startDate.setSeconds(0);
+];
+var startDate = new Date();
+function setMonthAndDay(startMonth, startDay) {
+    //一次設定好月份與日期
+    startDate.setMonth(startMonth - 1, startDay);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
 }
-setMonthAndDay(3,9);
+setMonthAndDay(3, 9);
